576a8cde02546a6ef7a8ebb45e208ed7ada60454
Francis Tseng <f+accounts@frnsys.com>
2016-09-11 14:19:10 -0400

    fixed libatlas3 pkg name

--- setup/math.sh
+++ setup/math.sh
@@ -1,1 +1,1 @@
-sudo apt-get install libatlas-base-dev liblapack-dev libopenblas-dev libopenblas-base libatlas3gf-base -y
+sudo apt-get install libatlas-base-dev liblapack-dev libopenblas-dev libopenblas-base libatlas3-base -y
