47e270fa5b19d66bd2c778b45534ee13f82f9c09
Francis Tseng <f+accounts@frnsys.com>
2016-11-26 12:51:12 -0500

    tmux 2.3

--- setup/util.sh
+++ setup/util.sh
@@ -4,8 +4,19 @@ tput setaf 5
 echo -e "\nInstalling utilties..."
 tput sgr0
 
-sudo apt-get install --no-install-recommends dos2unix tmux curl jq gnupg htop wget dnsutils imagemagick nmap httpie silversearcher-ag -y
+sudo apt-get install --no-install-recommends dos2unix curl jq gnupg htop wget dnsutils imagemagick nmap httpie silversearcher-ag -y
 sudo pip install youtube-dl
+
+# newer tmux
+sudo apt-get install libevent1-dev -y
+wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz /tmp/tmux-2.3.tar.gz
+cd /tmp
+tar -xzvf tmux-2.3.tar.gz
+cd tmux-*
+./configure
+make
+sudo make install
+cd $DIR
 
 git clone https://github.com/clvv/fasd.git /tmp/fasd
 cd /tmp/fasd
