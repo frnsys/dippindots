3acb5516845e41062fdef6e1bc4ab3c0b3912556
Francis Tseng <f+accounts@frnsys.com>
2018-12-22 16:10:42 -0500

    some better comments

--- setup/dots.sh
+++ setup/dots.sh
@@ -7,6 +7,8 @@ ln -sf $DIR/dots/bash/inputrc ~/.inputrc
 ln -sf $DIR/bin ~/.bin
 ln -sf $DIR/dots/tmux/tmux.conf ~/.tmux.conf
 
+# Check for environment file
+# NOTE: I have mine backed up in ~/docs
 if [ ! -f /etc/environment ]; then
     # Create an empty env file.
     echo "Creating an empty environment variables file at /etc/environment..."
