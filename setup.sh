8938e540227613cf1863c018ef09b464eff28bae
Francis Tseng <f+accounts@frnsys.com>
2023-01-03 17:11:15 -0500

    cleanup

--- setup.sh
+++ setup.sh
@@ -501,8 +501,8 @@ if [[ ! $APPS =~ ^[Yy]$ ]]; then
     sudo systemctl start vpn@ftseng.service
 
     # signal desktop client
-    wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-    cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+    wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > /tmp/signal-desktop-keyring.gpg
+    cat /tmp/signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
     echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
         sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
     sudo apt update && sudo apt install -y signal-desktop
