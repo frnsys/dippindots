a679ea7efb2368a7f62ae948c52f916994aff782
Francis Tseng <f+accounts@frnsys.com>
2024-11-20 17:49:12 +0100

    fix: many keyboard tweaks

--- dots/kbd/bkb-dilemma/config.h
+++ dots/kbd/bkb-dilemma/config.h
@@ -1,33 +1,27 @@
 #pragma once
 
-// bkb-dilemma-specific features.
-// #undef RGB_MATRIX_ENABLE
-// #undef ENCODER_ENABLE
-
-// Set the mouse settings to a comfortable speed/accuracy trade-off,
-// assuming a screen refresh rate of 60 Htz or higher
-// The default is 50. This makes the mouse ~3 times faster and more accurate
-#define MOUSEKEY_INTERVAL 16
-// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
-// give it more time to accelerate to max speed to retain precise control over short distances.
-#define MOUSEKEY_TIME_TO_MAX 40
-// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
-#define MOUSEKEY_DELAY 100
-// It makes sense to use the same delay for the mouseweel
-#define MOUSEKEY_WHEEL_DELAY 100
-// The default is 100
-#define MOUSEKEY_WHEEL_INTERVAL 50
-// The default is 40
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
-
 // Pointer sensitivity
-#define DILEMMA_MINIMUM_DEFAULT_DPI 300
+#define DILEMMA_MINIMUM_DEFAULT_DPI 200
+#define DILEMMA_DEFAULT_DPI_CONFIG_STEP 200
+#define DILEMMA_MINIMUM_SNIPING_DPI 200
+#define DILEMMA_SNIPING_DPI_CONFIG_STEP 100
 
-#define TAPPING_TERM 150
-#define PERMISSIVE_HOLD
-// #define HOLD_ON_OTHER_KEY_PRESS
+// How long the key must be pressed down to
+// be registered as a hold.
+//
+// Higher values mean it's harder to trigger
+// the hold function.
+#define TAPPING_TERM 165
+
+// Double-tap under this duration to trigger
+// hold-and-repeat.
 #define QUICK_TAP_TERM 120
-#define RETRO_TAPPING
 
-#define COMBO_TERM 30  // Milliseconds allowed to press combo keys
-#define COMBO_MUST_HOLD  // Require holding each combo key
+#define RETRO_TAPPING
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEYPRESS
+
+// Milliseconds allowed to press combo keys.
+// Lower if this if rolling keys often triggers combos.
+#define COMBO_TERM 30
+#define COMBO_MUST_HOLD_MODS // Require holding each combo key for mods
--- dots/kbd/bkb-dilemma/keymap.c
+++ dots/kbd/bkb-dilemma/keymap.c
@@ -8,18 +8,29 @@ enum layers {
 
 #define LAYOUT LAYOUT_split_3x5_3
 
-#define ____ KC_NO
-#define VVVV KC_TRNS
+#define ____ KC_NO // Unused
+#define XXXX KC_NO // Can't use; trigger for the current layer
+#define VVVV KC_TRNS // Passthrough
 
 // Thumb cluster keys
-#define TCR1 KC_LCTL
-#define TCR0 MT(MOD_LSFT, KC_BSPC)
-#define TCL1 LT(_NUMSYM, KC_SPACE)
-#define TCL0 LT(_NAVCTL, KC_ENTER)
+#define CTRL KC_LCTL
+#define SHFT KC_LSFT
+#define SPAC KC_SPACE
+#define ENAV LT(_NAVCTL, KC_ENTER)
+#define SSYM LT(_NUMSYM, KC_SPACE)
+#define SCTL MT(MOD_LCTL, KC_SPACE)
+#define BSFT MT(MOD_LSFT, KC_BSPC)
+#define ALTTAB LALT(KC_TAB)
+#define LSYM MO(_NUMSYM)
+#define GQUOT MT(MOD_LGUI, KC_QUOTE)
+
+#define DSYM LT(_NUMSYM, KC_D)
+#define KSYM LT(_NUMSYM, KC_K)
 
 // Mouse
-#define LCLIK MS_BTN1
-#define RCLIK MS_BTN2
+#define LCLK MS_BTN1
+#define RCLK MS_BTN2
+#define MCLK MS_BTN3
 
 // Symbols
 #define L_PARN LSFT(KC_9)     // (
@@ -36,6 +47,10 @@ enum layers {
 #define CARET LSFT(KC_6)      // ^
 #define AMPERSAND LSFT(KC_7)  // &
 #define ASTERISK LSFT(KC_8)   // *
+#define LTHAN LSFT(KC_COMMA)  // <
+#define GTHAN LSFT(KC_DOT)    // >
+#define QUOTE KC_QUOTE        // '
+#define DBLQT LSFT(KC_QUOTE)  // "
 
 // Window manager controls
 #define DESK1 LGUI(KC_1)
@@ -46,14 +61,14 @@ enum layers {
 #define BMENU LGUI(KC_SPACE)
 #define MAXIM LGUI(KC_F)       // Maximize window
 #define FLOAT LGUI(KC_S)       // Toggle floating window
+#define STICK LGUI(KC_Y)       // Toggle sticky window
 #define TODOS LGUI(KC_T)
 #define KPASS LGUI(KC_P)
 #define FOCWN LGUI(KC_H)       // Change window focus
+#define SINFO LGUI(KC_E)       // Show clock/battery notification
 #define FOCMN LGUI(LCTL(KC_K)) // Change monitor focus
 #define TOMON LGUI(LCTL(KC_M)) // Move window to monitor
-#define SINFO LGUI(KC_E)       // Show clock/battery notification
-#define SWARP LGUI(KC_B)       // Start warp
-#define CLOSE LGUI(LSFT(LALT(KC_Q))) // (Force) close window
+#define CLOSE LGUI(LSFT(LALT(KC_Q))) // (Force) close pindow
 
 // Terminal controls
 #define LTTAB LALT(KC_LBRC)    // Prev term tab
@@ -63,37 +78,35 @@ enum layers {
 #define FOCUS LALT(KC_W)       // Focus pane
 #define MXPAN LALT(KC_M)       // Maximize pane
 #define NTTAB LALT(KC_T)       // New term tab
+#define TSEAR LALT(KC_QUOTE)   // Terminal search
+#define THIST LALT(KC_E)       // Terminal scrollback history
+#define TNAME LALT(KC_COMMA)   // Terminal rename tab
+#define TRSZE LSFT(LALT(KC_R)) // Terminal resize window
 #define TCOPY LSFT(LALT(KC_C)) // Terminal copy
 #define TPAST LSFT(LALT(KC_P)) // Terminal paste
 #define TLINK LSFT(LALT(KC_E)) // Terminal open link
 #define TPATH LSFT(LALT(KC_O)) // Terminal copy path
-#define TSEAR LALT(KC_QUOTE)   // Terminal search
-#define THIST LALT(KC_E)       // Terminal scrollback history
-
-
-// #        define SNIPING SNIPING_MODE
-// #        define SNP_TOG SNIPING_MODE_TOGGLE
-// #        define DRGSCRL DRAGSCROLL_MODE
-// #        define DRG_TOG DRAGSCROLL_MODE_TOGGLE
+#define TWORD LSFT(LALT(KC_F)) // Terminal copy word
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ALPHA] = LAYOUT(
         KC_Q, KC_W, KC_E, KC_R, KC_T,       KC_Y, KC_U, KC_I,    KC_O,   KC_P,
-        KC_A, KC_S, KC_D, KC_F, KC_G,       KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,
+        KC_A, KC_S, DSYM, KC_F, KC_G,       KC_H, KC_J, KSYM,    KC_L,   KC_SCLN,
         KC_Z, KC_X, KC_C, KC_V, KC_B,       KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-                 SNP_TOG, TCR1, TCR0,       TCL0, TCL1, DRG_TOG
+                    LCLK, BSFT, QUOTE,      ENAV, SCTL, GQUOT
     ),
     [_NUMSYM] = LAYOUT(
-        KC_GRV, KC_1, KC_2, KC_3, CARET,    OCTHRP,  ____,    L_PARN,  R_PARN,  TILDE,
-        ____,   KC_4, KC_5, KC_6, DOLLAR,   AROBAS,  PERCENT, L_BRCE,  R_BRCE,  KC_EQUAL,
-        ____,   KC_7, KC_8, KC_9, KC_0,     ____,    ____,    KC_LBRC, KC_RBRC, PLUS,
-                      VVVV, VVVV, VVVV,     VVVV,    VVVV,    VVVV
+        KC_GRV, KC_1, KC_2, KC_3, DBLQT,    GTHAN,  PLUS,     L_PARN,  R_PARN,  TILDE,
+        KC_0,   KC_4, KC_5, KC_6, QUOTE,    LTHAN,  KC_EQUAL, L_BRCE,  R_BRCE,  EXCLAMAPT,
+        ____,   KC_7, KC_8, KC_9, ____,     OCTHRP, PERCENT,  KC_LBRC, KC_RBRC, AROBAS,
+                      VVVV, VVVV, VVVV,     VVVV,   XXXX,     VVVV
     ),
     [_NAVCTL] = LAYOUT(
-        KC_VOLD, KC_VOLU, MUSIC, PPLAY,   BMENU,       HSPLT, LTTAB,   KC_UP,   RTTAB,   TCOPY,
-        KC_LSFT, DESK1,   DESK2, DESK3,   FOCMN,       ____,  KC_LEFT, KC_DOWN, KC_RGHT, TPAST,
-        RCLIK,   FOCWN,   TODOS, LCLIK,   MAXIM,       THIST, MXPAN,   FOCUS,   TSEAR,   TPATH,
-                          VVVV,  KC_WBAK, KC_WFWD,     ____,  SINFO,   VVVV
+        // WM                                     // Terminal
+        KC_VOLD, KC_VOLU, MUSIC, PPLAY, BMENU,    HSPLT, LTTAB,   KC_UP,   RTTAB,   TPAST,
+        KC_LSFT, DESK1,   DESK2, DESK3, FOCMN,    ____,  KC_LEFT, KC_DOWN, KC_RGHT, TCOPY,
+        SNP_TOG, FOCWN,   TODOS, ____,  MAXIM,    THIST, MXPAN,   FOCUS,   TSEAR,   TWORD,
+                          ____,  ____,  ____,     XXXX,  ____,    ALTTAB
     ),
 };
 
@@ -105,23 +118,23 @@ const uint16_t PROGMEM pipe[]     = {KC_U, KC_P, COMBO_END};
 const uint16_t PROGMEM bslash[]   = {KC_Y, KC_P, COMBO_END};
 const uint16_t PROGMEM unscore[]  = {KC_M, KC_DOT, COMBO_END};
 const uint16_t PROGMEM colon[]    = {KC_J, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM quote[]    = {KC_J, KC_L, COMBO_END};
 const uint16_t PROGMEM ampers[]   = {KC_W, KC_R, COMBO_END};
-const uint16_t PROGMEM exclam[]   = {KC_Q, KC_E, COMBO_END};
 const uint16_t PROGMEM asterisk[] = {KC_Q, KC_R, COMBO_END};
 
 // Accented inputs
-const uint16_t PROGMEM accent_aigu[]   = {KC_E, KC_W, COMBO_END};
-const uint16_t PROGMEM accent_grave[]  = {KC_E, KC_R, COMBO_END};
-const uint16_t PROGMEM accent_cflex[]  = {KC_E, KC_R, KC_W, COMBO_END};
-const uint16_t PROGMEM accent_cdill[]  = {KC_X, KC_V, KC_C, COMBO_END};
+const uint16_t PROGMEM accent_aigu[]   = {KC_Q, KC_T, COMBO_END};
+const uint16_t PROGMEM accent_grave[]  = {KC_Q, KC_G, COMBO_END};
+const uint16_t PROGMEM accent_cflex[]  = {KC_Z, KC_G, COMBO_END};
+const uint16_t PROGMEM accent_cdill[]  = {KC_Z, KC_B, COMBO_END};
 
 // Terminal control combos
 const uint16_t PROGMEM term_new_tab[]  = {LTTAB, RTTAB, COMBO_END};
 
 // WM controls
 const uint16_t PROGMEM mute[] = {KC_VOLD, KC_VOLU, COMBO_END};
-const uint16_t PROGMEM warp[] = {LCLIK, RCLIK, COMBO_END};
+const uint16_t PROGMEM rclick[] = {KC_Z, LCLK, COMBO_END};
+const uint16_t PROGMEM mclick[] = {KC_A, LCLK, COMBO_END};
+const uint16_t PROGMEM show_info[] = {DESK1, DESK3, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(tab, KC_TAB),
@@ -129,11 +142,9 @@ combo_t key_combos[] = {
     COMBO(minus, KC_MINUS),
     COMBO(unscore, LSFT(KC_MINUS)),
     COMBO(colon, LSFT(KC_SCLN)),
-    COMBO(quote, KC_QUOTE),
     COMBO(pipe, LSFT(KC_BACKSLASH)),
     COMBO(bslash, KC_BACKSLASH),
     COMBO(ampers, AMPERSAND),
-    COMBO(exclam, EXCLAMAPT),
     COMBO(asterisk, ASTERISK),
 
     COMBO(accent_aigu, RALT(KC_QUOTE)),
@@ -144,28 +155,54 @@ combo_t key_combos[] = {
     COMBO(term_new_tab, NTTAB),
 
     COMBO(mute, KC_MUTE),
-    COMBO(warp, SWARP),
+    COMBO(rclick, RCLK),
+    COMBO(mclick, MCLK),
+    COMBO(show_info, SINFO),
 };
 
-const key_override_t wheel_up = ko_make_basic(MOD_MASK_SHIFT, KC_UP, MS_WHLU);
-const key_override_t wheel_down = ko_make_basic(MOD_MASK_SHIFT, KC_DOWN, MS_WHLD);
+// Vim
+const key_override_t dollar = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, DOLLAR);
+const key_override_t caret = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, CARET);
+
+// Browser
 const key_override_t next_browser_tab = ko_make_basic(MOD_MASK_SHIFT, RTTAB, LCTL(KC_TAB));
 const key_override_t prev_browser_tab = ko_make_basic(MOD_MASK_SHIFT, LTTAB, LCTL(LSFT(KC_TAB)));
+const key_override_t browser_forward = ko_make_basic(MOD_MASK_SHIFT, KC_RIGHT, KC_WFWD);
+const key_override_t browser_backward = ko_make_basic(MOD_MASK_SHIFT, KC_LEFT, KC_WBAK);
+
+// WM
 const key_override_t move_win_mon = ko_make_basic(MOD_MASK_SHIFT, FOCMN, TOMON);
 const key_override_t float_win = ko_make_basic(MOD_MASK_SHIFT, MAXIM, FLOAT);
+const key_override_t stick_win = ko_make_basic(MOD_MASK_SHIFT, FOCWN, STICK);
 const key_override_t close_win = ko_make_basic(MOD_MASK_SHIFT, BMENU, CLOSE);
+const key_override_t drag_win = ko_make_basic(MOD_MASK_SHIFT, LCLK, LGUI(LCLK));
 
-const key_override_t select_term_link = ko_make_basic(MOD_MASK_SHIFT, TPATH, TLINK);
+// System
+const key_override_t bright_up = ko_make_basic(MOD_MASK_SHIFT, KC_VOLU, KC_BRIGHTNESS_UP);
+const key_override_t bright_down = ko_make_basic(MOD_MASK_SHIFT, KC_VOLD, KC_BRIGHTNESS_DOWN);
+
+// Terminal
+const key_override_t select_term_path = ko_make_basic(MOD_MASK_SHIFT, TWORD, TPATH);
 const key_override_t term_v_split = ko_make_basic(MOD_MASK_SHIFT, HSPLT, VSPLT);
+const key_override_t term_name_tab = ko_make_basic(MOD_MASK_SHIFT, THIST, TNAME);
+const key_override_t term_resize_win = ko_make_basic(MOD_MASK_SHIFT, MXPAN, TRSZE);
 
 const key_override_t *key_overrides[] = {
-    &wheel_up,
-    &wheel_down,
+    &dollar,
+    &caret,
     &next_browser_tab,
     &prev_browser_tab,
-    &select_term_link,
+    &browser_forward,
+    &browser_backward,
+    &select_term_path,
     &term_v_split,
+    &term_name_tab,
+    &term_resize_win,
     &move_win_mon,
+    &drag_win,
     &float_win,
+    &stick_win,
     &close_win,
+    &bright_up,
+    &bright_down,
 };
