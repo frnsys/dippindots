415efe00efb565c4e2bfd27c978b863c354a68e7
Francis Tseng <accounts@frnsys.com>
2025-12-13 20:39:13 +0100

    better handling of keyboard firmware building (check for both /dev/sda and /dev/sdb) and higher debounce values

--- dots/kbd/flash.sh
+++ dots/kbd/flash.sh
@@ -3,7 +3,7 @@
 KIND=$1
 shift 1
 
-DISK="/dev/sda"
+DISK_OPTS=(/dev/sda /dev/sdb)
 MOUNT_POINT="/mnt/usb"
 
 set -e
@@ -11,12 +11,26 @@ set -e
 # Get sudo access
 sudo echo
 
+# Record which disks already exist
+declare -A present
+for d in "${DISK_OPTS[@]}"; do
+    if [ -e "$d" ]; then
+        present["$d"]=1
+    fi
+done
+
 # Last line is the firmware file.
 FIRMWARE=$(cd $KIND && ./build.sh "$@" | tee /dev/tty | tail -n1)
 echo "Firmware: $FIRMWARE"
 
-echo "Waiting for disk $DISK to become available..."
-while [ ! -e "$DISK" ]; do
+echo "Waiting for disk to become available..."
+while :; do
+    for d in "${DISK_OPTS[@]}"; do
+        if [ ! "${present[$d]}" ] && [ -e "$d" ]; then
+            DISK="$d"
+            break 2
+        fi
+    done
     sleep 1
 done
 
